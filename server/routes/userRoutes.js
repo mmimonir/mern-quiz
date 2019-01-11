06e156fd64fdb51b8c3d143f3947a6bdd970f4b6
mmimonir <monirdhk.it@gmail.com>
2019-01-11 21:27:43 +0600

    login route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
-const { register, activateAccount, getAllUsers, removeUser } = require("../controllers/userController");
+const { register, activateAccount, getAllUsers, removeUser, login } = require("../controllers/userController");
 
 
 router.post('/register', register)
 router.get('/activateAccount/:token', activateAccount)
 router.delete('/delete/:id', removeUser)
 router.get('/all', getAllUsers)
-
+router.post('/login', login)
 
 module.exports = router
